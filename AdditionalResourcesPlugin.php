be7c0c05f2255f0f7d93d73486fc3adbbd8d82ef
Franck Dupont <kyfr59@gmail.com>
2018-02-19 09:33:39 +0100

    First commit : plugin minimal structure

---
+++ AdditionalResourcesPlugin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * AdditionalResources
+ *
+ * Provides the ability to translate items in backoffice
+ *
+ * @copyright Copyright 2017-2020 Limonade & Co <technique@limonadeandco.fr>
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ * @package AdditionalResources
+ */
+
+// define('TRANSLATE_ITEMS_DIR', dirname(__FILE__));
+// define('JAVASCRIPT_ADMIN_DIR', WEB_PLUGIN.'/TranslateItems/views/admin/javascripts/');
+
+// require_once TRANSLATE_ITEMS_DIR . '/helpers/TranslateItems.php';
+
+/**
+ * The AdditionalResources plugin.
+ * @package Omeka\Plugins\AdditionalResources
+ */
+
+/**
+ * The AdditionalResources plugin.
+ * @package Omeka\Plugins\AdditionalResources
+ */
+class AdditionalResourcesPlugin extends Omeka_Plugin_AbstractPlugin
+{
+    /**
+     * @var array Hooks for the plugin.
+     */
+    protected $_hooks = array(
+    );
+
+
+    /**
+     * @var array Filters for the plugin.
+     */
+    protected $_filters = array();
+
+}
