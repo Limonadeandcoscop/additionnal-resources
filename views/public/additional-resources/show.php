37cc886fbf49d78375b1e74f9c273ceacc67aacb
Franck Dupont <kyfr59@gmail.com>
2018-02-20 09:23:39 +0100

    Display 'title' field on frontoffice

--- views/public/additional-resources/show.php
+++ views/public/additional-resources/show.php
@@ -5,15 +5,32 @@
 <?php else: ?>
 
 	<?php foreach ($resources as $resource): ?>
-		<p class="description"><?php echo $resource->description ?></p>
-		<?php $files = $resource->getFiles(); ?>
-		<?php if (count($files)): ?>
-			<?php foreach ($files as $file): ?>
-				<div class="pdf-file"><a href="<?php echo $file->getUrl() ?>" target="_blank"><?php echo $file->original_filename ?></a></div>
-			<?php endforeach; ?>				
-		<?php endif; ?>			
+		<div class="resource">
+			<?php $user = get_record_by_id("User", $resource->user_id); ?>
+			<p class="title"><strong><?php echo $resource->title ?></strong></p>
+			<p class="author">
+				<?php echo __('By') ?> : <?php echo $user->name ?>
+				<br /><span class="date"><?php echo $resource->created ?></span>
+			</p>
+			<?php if (strlen(trim($resource->description))): ?>
+				<p class="description"><?php echo $resource->description ?></p>
+			<?php endif; ?>				
+			<?php $files = $resource->getFiles(); ?>
+			<?php if (count($files)): ?>
+				<?php foreach ($files as $file): ?>
+					<div class="pdf-file"><a href="<?php echo $file->getUrl() ?>" target="_blank"><?php echo $file->original_filename ?></a></div>
+				<?php endforeach; ?>				
+			<?php endif; ?>			
+		</div>
 	<?php endforeach; ?>	
 
 <?php endif; ?>
 
+<style>
+.resource {
+	border:1px dotted #ccc; 
+	padding:0 10px;
+	margin-bottom:20px;
+}
+</style>
 <br /><br />
