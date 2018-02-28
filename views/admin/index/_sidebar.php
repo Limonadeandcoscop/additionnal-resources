9b074e61b543b5ef597bdd042fb35fcf193be1ad
Franck Dupont <kyfr59@gmail.com>
2018-02-28 11:08:02 +0100

    Give a link to original item for translations, on additional resources area in admin

--- views/admin/index/_sidebar.php
+++ views/admin/index/_sidebar.php
@@ -1,13 +1,24 @@
+<?php 
+	$translateItemTable = get_db()->getTable('TranslateItem');
+	$isOriginal = $translateItemTable->isOriginal($item); 
+	$itemLanguage = $translateItemTable->getLanguage($item);
+	$originalItem = $translateItemTable->getOriginalItem($item);
+?>
+
 <div id="harvester-duplicates" class="panel">
 	<h4><?php echo __('Additional resources') ?></h4>
-	<ul>
-	<?php foreach($resources as $resource): ?>
-	    <li>
-	    	<a href="<?php echo url('additional-resources/index/edit/resource_id/'.$resource->id) ?>"><?php echo cut_string($resource->title, 120) ?></a>
-	    </li>
-	<?php endforeach; ?>
-	</ul>
-	<br /><a href="<?php echo url('additional-resources/index/add/item_id/'.$item->id) ?>"><?php echo __('Add additional resource'); ?></a><br /><br />
+	<?php if ($isOriginal): ?>
+		<ul>
+		<?php foreach($resources as $resource): ?>
+		    <li>
+		    	<a href="<?php echo url('additional-resources/index/edit/resource_id/'.$resource->id) ?>"><?php echo cut_string($resource->title, 120) ?></a>
+		    </li>
+		<?php endforeach; ?>
+		</ul>
+		<br /><a href="<?php echo url('additional-resources/index/add/item_id/'.$item->id) ?>"><?php echo __('Add additional resource'); ?></a><br /><br />
+	<?php else: ?>	
+		<p><?php echo __('This item is a translation ('.getLanguageName($itemLanguage).'), switch to <a href="'.admin_url('/items/show/id/'.$originalItem->id).'">original item</a> to add resources'); ?></p>
+	<?php endif; ?>	
 </div>
 
 <?php if (OaipmhHarvesterPlugin::isTopLevelItem($item)): ?>
